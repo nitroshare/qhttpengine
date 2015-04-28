d06091431bbedd863ba637da668a7d1d0d5c8df6
Nathan Osman <nathan@quickmediasolutions.com>
2015-04-28 15:22:37 -0700

    Added QHttpRequest class.

---
+++ src/qhttprequest.h
@@ -0,0 +1,158 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef QHTTPENGINE_QHTTPREQUEST_H
+#define QHTTPENGINE_QHTTPREQUEST_H
+
+#include <QAbstractSocket>
+#include <QIODevice>
+#include <QList>
+
+#include "config.h"
+
+class QHTTPENGINE_EXPORT QHttpRequestPrivate;
+
+/**
+ * @brief The QHttpRequest class
+ */
+class QHTTPENGINE_EXPORT QHttpRequest : public QIODevice
+{
+    Q_OBJECT
+    Q_PROPERTY(Error error READ error NOTIFY errorChanged)
+    Q_PROPERTY(QByteArray method READ method)
+    Q_PROPERTY(QByteArray path READ path)
+    Q_PROPERTY(bool headersParsed READ headersParsed NOTIFY headersParsedChanged)
+    Q_ENUMS(Error)
+
+public:
+
+    /**
+     * @brief Error encountered while parsing request headers
+     */
+    enum Error {
+        NoError = 0,
+        MalformedRequestLine,
+        MalformedRequestHeader,
+        InvalidHttpVersion
+    };
+
+    /**
+     * @brief Create a new QHttpRequest from a QAbstractSocket
+     *
+     * It is assumed that the socket is already in the connected state and is
+     * open for reading.
+     */
+    QHttpRequest(QAbstractSocket *socket, QObject *parent = 0);
+
+    /**
+     * @brief Destroy the QHttpRequest
+     */
+    virtual ~QHttpRequest();
+
+    /**
+     * @brief Retrieve the number of bytes available for reading
+     */
+    virtual qint64 bytesAvailable() const;
+
+    /**
+     * @brief Determine if the device is sequential
+     *
+     * This method will always return true.
+     */
+    virtual bool isSequential() const;
+
+    /**
+     * @brief Retrieve the most recent error
+     */
+    Error error() const;
+
+    /**
+     * @brief Retrieve the request method
+     *
+     * This method may only be called after the request headers have been
+     * parsed.
+     */
+    QByteArray method() const;
+
+    /**
+     * @brief Retrieve the request path
+     *
+     * This method may only be called after the request headers have been
+     * parsed.
+     */
+    QByteArray path() const;
+
+    /**
+     * @brief Determine if request headers have been parsed yet
+     */
+    bool headersParsed() const;
+
+    /**
+     * @brief Retrieve a list of request headers
+     *
+     * This method may only be called after the request headers have been
+     * parsed.
+     */
+    Q_INVOKABLE QList<QByteArray> headers() const;
+
+    /**
+     * @brief Retrieve the value of a specific request header
+     *
+     * This method may only be called after the request headers have been
+     * parsed. Headers are case-insensitive. If the specified header was not
+     * set, then a null byte array is returned.
+     */
+    Q_INVOKABLE QByteArray header(const QByteArray &name) const;
+
+Q_SIGNALS:
+
+    /**
+     * @brief Indicate that a parsing error has occurred
+     *
+     * Any attempts to read from the device after this point may fail. A brief
+     * description of the error condition can be retrieved with the
+     * errorString() method.
+     */
+    void errorChanged(Error error);
+
+    /**
+     * @brief Indicate that request headers have been parsed
+     *
+     * Once this signal is emitted, it is safe to begin reading request data.
+     * The readyRead() signal will be emitted as request data is received.
+     */
+    void headersParsedChanged(bool headersParsed);
+
+private:
+
+    virtual qint64 readData(char *data, qint64 maxlen);
+    virtual qint64 writeData(const char *data, qint64 len);
+
+    QHttpRequestPrivate *const d;
+    friend class QHttpRequestPrivate;
+};
+
+Q_DECLARE_METATYPE(QHttpRequest::Error)
+
+#endif // QHTTPENGINE_QHTTPREQUEST_H
---
+++ src/qhttprequest_p.h
@@ -0,0 +1,68 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef QHTTPENGINE_QHTTPREQUESTPRIVATE_H
+#define QHTTPENGINE_QHTTPREQUESTPRIVATE_H
+
+#include <QAbstractSocket>
+#include <QMap>
+#include <QObject>
+
+#include "qhttprequest.h"
+
+class QHttpRequestPrivate : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    QHttpRequestPrivate(QHttpRequest *request, QAbstractSocket *baseSocket);
+
+    QAbstractSocket *const socket;
+
+    QHttpRequest::Error error;
+    QByteArray method;
+    QByteArray path;
+
+    QMap<QByteArray, QByteArray> headers;
+    bool headersParsed;
+
+    QByteArray buffer;
+
+private Q_SLOTS:
+
+    void onReadyRead();
+
+private:
+
+    QList<QByteArray> split(const QByteArray &data, const QByteArray &delim);
+
+    void parseHeaders(const QByteArray &data);
+    void parseStatusLine(const QByteArray &line);
+    void parseHeader(const QByteArray &line);
+
+    QHttpRequest *const q;
+};
+
+#endif // QHTTPENGINE_QHTTPREQUESTPRIVATE_H
